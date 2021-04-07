6fbf877deb95407f458583650375a26a9a655026
Filipe Barbosa <up201909573@fe.up.pt>
2021-04-07 15:43:22 +0100

    Fix initial lint errors

---
+++ fe/src/Pages/Calculator.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // This is useless for now, but i think that we will have states later on, so ignore the warnings
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Calculator Page</h1>
+      </>
+    );
+  }
+}
+
+export default Calculator;
---
+++ fe/src/Pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <>
+      <div style={{ marginTop: '8em', paddingLeft: '8em' }}>
+        <h1 style={{ color: 'white', fontWeight: 'bold', fontSize: '9em' }}>Secure</h1>
+        <h1 style={{ color: 'white', fontWeight: 'bold', fontSize: '9em' }}>Connection</h1>
+        <h3 style={{ fontSize: '4rem', paddingTop: '0.5em' }}>Expertise. Commitment. Value</h3>
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
