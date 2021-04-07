4d5d39d2b25191e8200a53d9a9ed5167b287bc7f
AndreiaGouveia <andreiagouveia1@gmail.com>
2021-04-07 15:05:18 +0100

    Added homepage

--- fe/src/Pages/HomePage.js
+++ fe/src/Pages/HomePage.js
@@ -1,6 +1,12 @@
 function HomePage() {
   return (
-    <h1>HomePage</h1>
+    <>
+      <div style={{marginTop: '8em' , paddingLeft: '8em'}}>
+        <h1 style={{color:'white', fontWeight:'bold', fontSize:'9em'}}>Secure</h1>
+        <h1 style={{color:'white', fontWeight:'bold', fontSize:'9em'}}>Connection</h1>
+        <h3 style={{fontSize:'4rem', paddingTop:'0.5em'}}>Expertise. Commitment. Value</h3>
+      </div>  
+    </>  
   );
 }
 
