8eb50066ad09e4a37cb79570f0d54e502ac5f06b
AndreiaGouveia <andreiagouveia1@gmail.com>
2021-04-09 00:06:36 +0100

    Added some stuff to calculator files

--- fe/src/Pages/Calculator.jsx
+++ fe/src/Pages/Calculator.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEquals } from '@fortawesome/free-solid-svg-icons';
+import { Form, Button } from 'react-bootstrap';
 
 class Calculator extends React.Component {
 	constructor(props) {
@@ -10,7 +13,26 @@ class Calculator extends React.Component {
 	render() {
 		return (
 			<>
-				<h1>Calculator Page</h1>
+				<div id="content">
+					<h1 style={{ color: 'white', fontWeight: 'bold', fontSize: '6vmax' }}>Operations</h1>
+					<div id="calculatorContent">
+						<div id='calculatorInput'>
+							<Form id="login-form">
+								<Form.Group controlId="formUsername">
+									<Form.Control type="text" placeholder="Username" maxLength="8" />
+								</Form.Group>
+
+								<Form.Group controlId="formID">
+									<Form.Control type="text" placeholder="ID" maxLength="12" minLength="12" pattern="([A-Z]|[a-z]|[0-9])+" />
+								</Form.Group>
+								<Button variant="primary" type="submit">
+											Log In
+								</Button>
+							</Form>
+						</div>
+						<FontAwesomeIcon style={{color:'white' , fontSize:'7vmax'}} icon={faEquals} />
+					</div>
+				</div>
 			</>
 		);
 	}
