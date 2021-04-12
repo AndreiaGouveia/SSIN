e42ebfd7fe46e1db83a152d2d2e7069dfe849f5f
AndreiaGouveia <andreiagouveia1@gmail.com>
2021-04-12 18:52:22 +0100

    Progress in calculator page

--- fe/src/Pages/Calculator.jsx
+++ fe/src/Pages/Calculator.jsx
@@ -1,41 +1,46 @@
-import React from 'react';
+import React , {useEffect, useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEquals } from '@fortawesome/free-solid-svg-icons';
 import { Form, Button } from 'react-bootstrap';
+import '../CSS/Calculator.css';
 
-class Calculator extends React.Component {
-	constructor(props) {
-		super(props);
+function Calculator () {
 
-		// This is useless for now, but i think that we will have states later on, so ignore the warnings
-	}
+	const [loading, setLoading] = useState(true);
+	const [data, setData] = useState({});
+	const [value, setValue] = useState('hello'),
+		onInput = ({target:{value}}) => setValue(value),
+		onFormSubmit = e => {
+			e.preventDefault();
+			console.log(value);
+			setValue();
+		};
+	
+	useEffect
 
-	render() {
-		return (
-			<>
-				<div id="content">
-					<h1 style={{ color: 'white', fontWeight: 'bold', fontSize: '6vmax' }}>Operations</h1>
-					<div id="calculatorContent">
-						<div id='calculatorInput'>
-							<Form id="login-form">
-								<Form.Group controlId="formUsername">
-									<Form.Control type="text" placeholder="Username" maxLength="8" />
-								</Form.Group>
-
-								<Form.Group controlId="formID">
-									<Form.Control type="text" placeholder="ID" maxLength="12" minLength="12" pattern="([A-Z]|[a-z]|[0-9])+" />
-								</Form.Group>
-								<Button variant="primary" type="submit">
-											Log In
-								</Button>
-							</Form>
-						</div>
-						<FontAwesomeIcon style={{color:'white' , fontSize:'7vmax'}} icon={faEquals} />
+	return (
+		<>
+			<div id="content">
+				<h1 style={{ color: 'white', fontWeight: 'bold', fontSize: '6vmax' }}>Operations</h1>
+				<div id="calculatorContent">
+					<div id="calculatorInput">
+						<Form id="calculator-form">
+							<Form.Group controlId="formCalculator">
+								<Form.Control type="text" placeholder="Calculator" maxLength="8" />
+							</Form.Group>
+							<Button variant="primary" type="submit">
+									Submit
+							</Button>
+						</Form>
+					</div>
+					<FontAwesomeIcon style={{color:'#48cfac' , fontSize:'15vmax', marginTop: '10vmax'}} icon={faEquals} />
+					<div id="calculatorOutput">
+						<h1>{value}</h1>
 					</div>
 				</div>
-			</>
-		);
-	}
+			</div>
+		</>
+	);
 }
 
 export default Calculator;
