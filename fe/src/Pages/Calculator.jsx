f29ec38dcd045526cf7a96bf343f24c377c1c1d7
AndreiaGouveia <andreiagouveia1@gmail.com>
2021-05-15 15:14:37 +0100

    Calculator almost finnished

--- fe/src/Pages/Calculator.jsx
+++ fe/src/Pages/Calculator.jsx
@@ -1,46 +1,70 @@
-import React , {useEffect, useState} from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEquals } from '@fortawesome/free-solid-svg-icons';
 import { Form, Button } from 'react-bootstrap';
 import '../CSS/Calculator.css';
 
-function Calculator () {
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      data: {},
+      value: '',
+      response: '',
+    };
+  }
 
-	const [loading, setLoading] = useState(true);
-	const [data, setData] = useState({});
-	const [value, setValue] = useState('hello'),
-		onInput = ({target:{value}}) => setValue(value),
-		onFormSubmit = e => {
-			e.preventDefault();
-			console.log(value);
-			setValue();
-		};
-	
-	useEffect
+  onSubmit(value) {
+    /*setResponse(value);*/
+    console.log('here');
+    console.log(value);
+  }
 
-	return (
-		<>
-			<div id="content">
-				<h1 style={{ color: 'white', fontWeight: 'bold', fontSize: '6vmax' }}>Operations</h1>
-				<div id="calculatorContent">
-					<div id="calculatorInput">
-						<Form id="calculator-form">
-							<Form.Group controlId="formCalculator">
-								<Form.Control type="text" placeholder="Calculator" maxLength="8" />
-							</Form.Group>
-							<Button variant="primary" type="submit">
-									Submit
-							</Button>
-						</Form>
-					</div>
-					<FontAwesomeIcon style={{color:'#48cfac' , fontSize:'15vmax', marginTop: '10vmax'}} icon={faEquals} />
-					<div id="calculatorOutput">
-						<h1>{value}</h1>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  render() {
+	console.log(this.state.value);
+    return (
+      <>
+        <div id='content'>
+          <h1 style={{ color: 'white', fontWeight: 'bold', fontSize: '6vmax' }}>
+            Operations
+          </h1>
+          <div id='calculatorContent'>
+            <div id='calculatorInput'>
+              <Form id='calculator-form'>
+                <Form.Group controlId='formCalculator'>
+                  <Form.Control
+                    type='text'
+                    as='textarea'
+                    placeholder='Operation...'
+                    onChange={(e) => this.setState({value: e.target.value})}
+                  />
+                </Form.Group>
+                <Button
+                  variant='primary'
+                  type='submit'
+                  onClick={this.onSubmit}
+                >
+                  Submit
+                </Button>
+              </Form>
+            </div>
+            <FontAwesomeIcon
+              style={{
+                color: '#48cfac',
+                fontSize: '15vmax',
+                marginTop: '10vmax',
+              }}
+              icon={faEquals}
+            />
+            <div id='calculatorOutput'>
+              <h1>{this.state.response}</h1>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Calculator;
