fbf9241461d16fececca88392943a0608e1019c2
Filipe Barbosa <up201909573@edu.fe.up.pt>
2021-05-16 21:35:13 +0100

    Add PeerJS server to server

--- server/server.mjs
+++ server/server.mjs
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import routes from './routes/index.mjs';
-import { connectDb } from './models/index.mjs'
+import { connectDb } from './models/index.mjs';
+import { ExpressPeerServer } from 'peer';
 
 dotenv.config();
 const bparser_form = express.json()
@@ -9,16 +10,30 @@ const app = express();
 
 app.use(bparser_form)
 
+// Enable CORS (for local testing only -remove in production/deployment)
+app.use((req, res, next) => {
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept');
+   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+   next();
+});
+
 app.use('/', routes);
-app.get('*', (req, res) => res.status(200).send({
-   message: 'There is nothing here',
- }));
- 
+
 connectDb().then(() => {
    let server = app.listen(process.env.SERVER_PORT || 8080, function () {
       let port = server.address().port
       console.log("Welcome to the server. Running in port %s", port)
-   })
+   });
+
+   app.use('/peerjs', ExpressPeerServer(server, {
+      debug: true,
+      path: '/'
+   }));
+
+   app.get('*', (req, res) => res.status(200).send({
+      message: 'There is nothing here',
+   }));
 });
 
 export default app;
