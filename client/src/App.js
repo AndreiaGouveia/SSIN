fb7a6a666ecc42742c54efe5a76dcc8fc1906194
AndreiaGouveia <andreiagouveia1@gmail.com>
2021-05-20 01:30:40 +0100

    feat: register

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import {
-  BrowserRouter, Route, Switch,
-} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import LogIn from './Pages/LogIn';
 import Calculator from './Pages/Calculator';
 import Messages from './Pages/Messages';
 import Profile from './Pages/Profile';
 import Layout from './Components/Layout';
-
+import { getUser } from './auth.js';
 import './App.css';
 
 function App() {
@@ -16,11 +14,19 @@ function App() {
     <Layout>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/login" component={LogIn} />
-          <Route exact path="/messages" component={Messages} />
-          <Route exact path="/calculator" component={Calculator} />
-          <Route exact path="/profile" component={Profile} />
-          <Route exact path="/" component={HomePage} />
+          {getUser().username === null ? (
+            <>
+              <Route exact path='/login' component={LogIn} />{' '}
+              <Route exact path='/' component={HomePage} />
+            </>
+          ) : (
+            <>
+              <Route exact path='/messages' component={Messages} />
+              <Route exact path='/calculator' component={Calculator} />
+              <Route exact path='/profile' component={Profile} />
+              <Route exact path='/' component={HomePage} />
+            </>
+          )}
         </Switch>
       </BrowserRouter>
     </Layout>
--- client/src/Pages/LogIn.jsx
+++ client/src/Pages/LogIn.jsx
@@ -2,57 +2,76 @@ import React from 'react';
 import { Form, Button } from 'react-bootstrap';
 import HomePage from './HomePage';
 import './../CSS/LogIn.css';
-import { login, getUser } from '../auth.js';
+import { register } from '../auth.js';
+import { Redirect } from 'react-router-dom';
 
 class LogIn extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		// This is useless for now, but i think that we will have states later on, so ignore the warnings
+    this.state = {
+      redirect: false,
+    };
 
-		this.performLogIn = this.performLogIn.bind(this);
-	}
+    // This is useless for now, but i think that we will have states later on, so ignore the warnings
 
-	performLogIn(event){
-		console.log('loging in..');
-		event.preventDefault();
+    this.performLogIn = this.performLogIn.bind(this);
+  }
 
-		let username = event.target[0].value;
-		let id = event.target[0].value;
-		let publicKey= 'sou a public key';
-		let privateKey='sou a private key';
+  performLogIn(event) {
+    let username = event.target[0].value;
+    let id = event.target[1].value;
 
-		login(username,id,publicKey,privateKey);
-	}
-	render() {
-		console.log(getUser());
-		return (
-			<>
-				<div id="login">
-					<HomePage />
+	//to do : inserir função para buscar as keys
+    let publicKey = 'sou a public key';
+    let privateKey = 'sou a private key';
 
-					<div className="container h-100">
-						<div className="row h-100 justify-content-center align-items-center">
-							<div className="col-10 col-md-8 col-lg-6">
-								<Form id="login-form" onSubmit={this.performLogIn} >
-									<Form.Group controlId="formUsername">
-										<Form.Control type="text" placeholder="Username" maxLength="8" />
-									</Form.Group>
+    if (register(username, id, publicKey, privateKey)) {
+      this.setState({ redirect: true });
+    }
+  }
+  render() {
+    if (!this.state.redirect) {
+      return (
+        <>
+          <div id='login'>
+            <HomePage />
 
-									<Form.Group controlId="formID">
-										<Form.Control type="text" placeholder="ID" maxLength="12" minLength="12" pattern="([A-Z]|[a-z]|[0-9])+" />
-									</Form.Group>
-									<Button variant='primary' type='submit' >
-										Log In
-									</Button>
-								</Form>
-							</div>
-						</div>
-					</div>
-				</div>
-			</>
-		);
-	}
+            <div className='container h-100'>
+              <div className='row h-100 justify-content-center align-items-center'>
+                <div className='col-10 col-md-8 col-lg-6'>
+                  <Form id='login-form' onSubmit={this.performLogIn}>
+                    <Form.Group controlId='formUsername'>
+                      <Form.Control
+                        type='text'
+                        placeholder='Username'
+                        maxLength='8'
+                      />
+                    </Form.Group>
+
+                    <Form.Group controlId='formID'>
+                      <Form.Control
+                        type='text'
+                        placeholder='ID'
+                        maxLength='12'
+                        minLength='12'
+                        pattern='([A-Z]|[a-z]|[0-9])+'
+                      />
+                    </Form.Group>
+                    <Button variant='primary' type='submit'>
+                      Log In
+                    </Button>
+                  </Form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      );
+    } else {
+      return <Redirect push to='/' />;
+    }
+  }
 }
 
 export default LogIn;
